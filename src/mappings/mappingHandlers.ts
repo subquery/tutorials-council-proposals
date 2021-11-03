c7431288e2b6684664014fb747df25cb89474d43
James Bayly <bayly.james@gmail.com>
2021-11-04 11:22:49 +1300

    Improvements to add councillor

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,39 +1,51 @@
-import { SubstrateEvent } from '@subql/types'
-import { Proposals, Votes } from '../types/models'
+import { SubstrateEvent } from "@subql/types";
+import { Proposal, Vote, Councillor } from "../types/models";
 
 export async function handleCouncilProposedEvent(
-	event: SubstrateEvent
+  event: SubstrateEvent
 ): Promise<void> {
-	const {
-		event: {
-			data: [accountId, proposal_index, proposal_hash, threshold],
-		},
-	} = event
-	// Retrieve the record by the accountID
-	const record = new Proposals(accountId.toString())
-	record.index = proposal_index.toString()
-	record.account = accountId.toString()
-	record.hash = proposal_hash.toString()
-	record.voteThreshold = threshold.toString()
-	record.block = event.block.block.header.number.toBigInt()
-	await record.save()
+  const {
+    event: {
+      data: [accountId, proposal_index, proposal_hash, threshold],
+    },
+  } = event;
+  // Retrieve the record by the accountID
+  const proposal = new Proposal(accountId.toString());
+  proposal.index = proposal_index.toString();
+  proposal.account = accountId.toString();
+  proposal.hash = proposal_hash.toString();
+  proposal.voteThreshold = threshold.toString();
+  proposal.block = event.block.block.header.number.toBigInt();
+  await proposal.save();
 }
 
 export async function handleCouncilVotedEvent(
-	event: SubstrateEvent
+  event: SubstrateEvent
 ): Promise<void> {
-	const {
-		event: {
-			data: [accountId, proposal_hash, approved_vote, numberYes, numberNo],
-		},
-	} = event
-	// Retrieve the record by the accountID
-	const record = new Votes(accountId.toString())
-	record.proposalHash = proposal_hash.toString()
-	record.approvedVote = approved_vote.toString()
-	record.account = accountId.toString()
-	record.votedYes = numberYes.toString()
-	record.votedNo = numberNo.toString()
-	record.block = event.block.block.header.number.toBigInt()
-	await record.save()
+  const {
+    event: {
+      data: [accountId, proposal_hash, approved_vote, numberYes, numberNo],
+    },
+  } = event;
+
+  ensureCouncillor(accountId.toString());
+  // Retrieve the record by the accountID
+  const vote = new Vote(accountId.toString());
+  vote.proposalHashId = proposal_hash.toString();
+  vote.approvedVote = approved_vote.toString();
+  vote.councillorId = accountId.toString();
+  vote.votedYes = numberYes.toString();
+  vote.votedNo = numberNo.toString();
+  vote.block = event.block.block.header.number.toBigInt();
+  await vote.save();
+}
+
+async function ensureCouncillor(accountId: string): Promise<void> {
+  // ensure that our account entities exist
+  let councillor = await Councillor.get(accountId);
+  if (!councillor) {
+    councillor = new Councillor(accountId);
+  }
+  councillor.numberOfVotes += 1;
+  await councillor.save();
 }
